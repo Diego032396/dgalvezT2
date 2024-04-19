835d69c6e8c60900659fe7bd2f809e74fe6b960a
Diego032396 <diegojosuegalvez@gmail.com>
2024-04-19 11:31:25 -0500

    final

--- Vistas/vLogin.xaml.cs
+++ Vistas/vLogin.xaml.cs
@@ -2,6 +2,8 @@ namespace dgalvezT2.Vistas;
 
 public partial class vLogin : ContentPage
 {
+	string[] usuarios = { "Carlos", "Ana", "Jose" };
+	string[] contrasenas = {"carlos123","ana123","jose123"};
 	public vLogin()
 	{
 		InitializeComponent();
@@ -9,6 +11,25 @@ public partial class vLogin : ContentPage
 
     private void btnInicio_Clicked(object sender, EventArgs e)
     {
+		string usuarioIngresado = txtUsario.Text;
+		string contrasenaIngresada = txtContrasena.Text;
 
+		bool credencialesValidas = false;
+
+		for (int i = 0; i < usuarios.Length; i++)
+		{
+			if (usuarios[i] == usuarioIngresado && contrasenas[i] == contrasenaIngresada)
+			{
+				credencialesValidas = true;
+				DisplayAlert("bienvenido", "ingresaste corectamento" + usuarioIngresado, "ok");
+				Navigation.PushAsync(new MainPage());
+			}
+		}
+		if(!credencialesValidas)
+		{
+			DisplayAlert("Error", "usuario o contraseña incorrectos", "ok");
+		}
+		
+		
     }
 }
